3740aa0a8f8380212cff6f2b6c4d4f65f5c8eee0
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-12 16:59:41 +0530

    Remove Google Container Cluster data source
    Removed configuration for the Google Container Cluster.

--- terraform/environments/dev/data.tf
+++ terraform/environments/dev/data.tf
@@ -1,8 +1,3 @@
 # data.tf
-data "google_container_cluster" "mycluster" {
-  name     = "ecom-dev-cluster"     # Replace with your cluster name
-  location = "us-central1"    # Replace with your cluster zone
-  project  = "proj-ecom-dev"    # Your GCP project
-}
 
 data "google_client_config" "default" {}
