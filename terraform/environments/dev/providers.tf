83f6cf6636eeb0688b08ed78f4eb7a8381e88cb2
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-12 17:00:37 +0530

    Update Kubernetes provider configuration for dev

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -1,8 +1,7 @@
-# providers.tf
 provider "kubernetes" {
-  host                   = data.google_container_cluster.mycluster.endpoint
+  host                   = google_container_cluster.dev.endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.mycluster.master_auth[0].cluster_ca_certificate
+    google_container_cluster.dev.master_auth[0].cluster_ca_certificate
   )
 }
