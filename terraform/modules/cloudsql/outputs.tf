1e2c39b8175f521e2e1fa9ed556109fb15b44a6f
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-12 07:36:01 +0530

    Update Cloud SQL outputs to use 'postgres' instance

--- terraform/modules/cloudsql/outputs.tf
+++ terraform/modules/cloudsql/outputs.tf
@@ -1,7 +1,7 @@
 output "db_instance_connection_name" {
-  value = google_sql_database_instance.saleor_postgres.connection_name
+  value = google_sql_database_instance.postgres.connection_name
 }
 
 output "db_ip" {
-  value = google_sql_database_instance.saleor_postgres.public_ip_address
+  value = google_sql_database_instance.postgres.public_ip_address
 }
