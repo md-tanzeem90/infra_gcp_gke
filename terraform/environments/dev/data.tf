e486b62cffddd156beabbdcbbcf7bec523776cee
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-12 16:51:13 +0530

    Update GCP project reference in data.tf

--- terraform/environments/dev/data.tf
+++ terraform/environments/dev/data.tf
@@ -1,10 +1,8 @@
 # data.tf
-data "google_project" "project" {}
-
 data "google_container_cluster" "mycluster" {
   name     = "ecom-dev-cluster"     # Replace with your cluster name
   location = "us-central1"    # Replace with your cluster zone
-  project  = data.google_project.project.project_id
+  project  = "proj-ecom-dev"    # Your GCP project
 }
 
 data "google_client_config" "default" {}
