8c97441d0580e513b174cdc48f5308515a9cf999
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 03:51:37 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,3 @@
-# providers.tf
-
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -7,25 +5,16 @@ provider "google" {
 
 data "google_client_config" "default" {}
 
-# ✅ Kubernetes Provider
 provider "kubernetes" {
-  host                   = module.gke.cluster_endpoint
+  host                   = "https://${module.gke.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-  # ensures cluster exists before provider init
-  experiments {
-    manifest_resource = true
-  }
 }
 
-# ✅ Helm Provider (reuse same config)
 provider "helm" {
   kubernetes {
-    host                   = module.gke.cluster_endpoint
+    host                   = "https://${module.gke.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
-   # ensures cluster exists before provider init
-  experiments {
-    manifest_resource = true
   }
 }
