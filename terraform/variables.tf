299b7912ee7141d9fc41b47dffb7c8591a8c92bd
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 02:31:37 +0530

    Add variables for environment, project_id, and region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,14 @@
+variable "environment" {
+  description = "Environment derived from branch"
+  type        = string
+}
 
+variable "project_id" {
+  description = "Dynamic project id"
+  type        = string
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
