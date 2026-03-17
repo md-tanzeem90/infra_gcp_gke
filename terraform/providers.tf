4eb10e9416c37b6be8cacfe980f74adf1d0459bc
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 02:40:51 +0530

    Add Google provider configuration to providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,13 @@
 # providers.tf
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 provider "kubernetes" {
   host                   = "https://${module.gke.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
 }
+
