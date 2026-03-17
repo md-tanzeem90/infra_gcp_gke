4d13c4eec4a8251a430fd7b1128255630d7f91e8
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 03:05:46 +0530

    Parameterize VPC and subnet names with environment

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,23 +1,25 @@
 resource "google_compute_network" "vpc" {
-  name                    = "ecom-vpc"
+  name                    = "ecom-${var.environment}-vpc"
+  project                 = var.project_id
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "dev" {
-  name          = "ecom-dev-subnet"
-  ip_cidr_range = "10.10.0.0/20"
-  region        = "us-central1"
+resource "google_compute_subnetwork" "subnet" {
+  name          = "ecom-${var.environment}-subnet"
+  ip_cidr_range = local.subnet_cidr
+  region        = var.region
+  project       = var.project_id
   network       = google_compute_network.vpc.id
 
   private_ip_google_access = true
 
   secondary_ip_range {
     range_name    = "pods"
-    ip_cidr_range = "10.20.0.0/16"
+    ip_cidr_range = local.pods_cidr
   }
 
   secondary_ip_range {
     range_name    = "services"
-    ip_cidr_range = "10.30.0.0/20"
+    ip_cidr_range = local.services_cidr
   }
 }
