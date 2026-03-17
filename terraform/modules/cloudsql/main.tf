41099f5973c7c57c33f1c8888eaa084eae64c841
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 02:38:54 +0530

    Refactor Cloud SQL resource names using locals

--- terraform/modules/cloudsql/main.tf
+++ terraform/modules/cloudsql/main.tf
@@ -1,5 +1,17 @@
+########################################
+# Locals (naming convention)
+########################################
+locals {
+  db_instance_name = "postgres-${var.environment}"
+  db_name          = "postgres_app_db_${var.environment}"
+  db_user          = "db_user_${var.environment}"
+}
+
+########################################
+# Cloud SQL Instance
+########################################
 resource "google_sql_database_instance" "postgres" {
-  name             = "postgres"
+  name             = local.db_instance_name
   region           = var.region
   database_version = "POSTGRES_14"
 
@@ -8,19 +20,35 @@ resource "google_sql_database_instance" "postgres" {
 
     ip_configuration {
       ipv4_enabled = true
+
+      #  Restrict later (for now open for testing)
+      authorized_networks {
+        name  = "allow-all-temp"
+        value = "0.0.0.0/0"
+      }
     }
   }
 
-  deletion_protection = false
+  deletion_protection = var.environment == "prod" ? true : false
+
+  lifecycle {
+    prevent_destroy = var.environment == "prod"
+  }
 }
 
+########################################
+# Database
+########################################
 resource "google_sql_database" "postgres_db" {
-  name     = "postgres_app_db"
+  name     = local.db_name
   instance = google_sql_database_instance.postgres.name
 }
 
+########################################
+# DB User
+########################################
 resource "google_sql_user" "db_user" {
-  name     = "db_user"
+  name     = local.db_user
   instance = google_sql_database_instance.postgres.name
   password = var.db_password
 }
