e2b8be72be7d7b5985607d2c767ac509955233d6
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 03:40:08 +0530

    Update outputs.tf

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -1,11 +1,7 @@
 output "cluster_endpoint" {
-  value = google_container_cluster.dev.endpoint
+  value = google_container_cluster.primary.endpoint
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.dev.master_auth[0].cluster_ca_certificate
-}
-
-output "cluster_name" {
-  value = google_container_cluster.dev.name
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
 }
