b8377af6a9d58565ed4c24d415242f046f927c76
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-18 02:58:02 +0530

    Define GKE module variables with descriptions
    Added variables for environment, project_id, region, network, and subnetwork with descriptions.

--- terraform/modules/gke/variables.tf
+++ terraform/modules/gke/variables.tf
@@ -1,2 +1,25 @@
-variable "network" {}
-variable "subnetwork" {}
+variable "environment" {
+  description = "Environment name derived from branch"
+  type        = string
+}
+
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "network" {
+  description = "VPC network"
+  type        = string
+}
+
+variable "subnetwork" {
+  description = "Subnetwork"
+  type        = string
+}
