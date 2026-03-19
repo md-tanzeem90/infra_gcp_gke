cfe933b3ddbfb0e3566f614f2a4c5d60ea93f5df
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-19 18:11:01 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,9 +12,10 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${module.gke.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
   }
+}
 }
