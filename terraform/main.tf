e16f07c058a2599bee94d4b7ef641dff70f394e8
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-19 17:45:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,13 +25,6 @@ resource "google_project_service" "services" {
   service = each.key
 }
 
-resource "google_project" "project" {
-  project_id      = var.project_id
-  name            = var.project_id
-  org_id          = var.org_id
-  billing_account = var.billing_account
-}
-
 module "network" {
   source = "./modules/network"
 
