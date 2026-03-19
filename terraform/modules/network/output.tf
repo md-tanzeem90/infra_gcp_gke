2451462edbefcb792ac4616a1a35ad574540c8fd
md-tanzeem90 <30793591+md-tanzeem90@users.noreply.github.com>
2026-03-19 19:11:13 +0530

    Update output.tf

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -7,9 +7,9 @@ output "vpc_id" {
 }
 
 output "subnet_name" {
-  value = google_compute_subnetwork.dev.name
+  value = google_compute_subnetwork.subnet.name
 }
 
 output "subnet_id" {
-  value = google_compute_subnetwork.dev.id
+  value = google_compute_subnetwork.subnet.id
 }
